b5eb3bacb9bcb2352200449af581f59e8616f0ed
sturay <stuart_harding@me.com>
2016-07-27 08:24:31 +0100

    Stuff added for linting

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,61 +1,61 @@
 // Specify the target button to collapse/expand the main navigation
 var nav = document.getElementById('toggleBtn'); // <nav class="nav">
 // initial heights
-var initialHeights = document.getElementsByClassName("level-1")
-var values = []
-this.tooltipId = undefined
+var initialHeights = document.getElementsByClassName("level-1");
+var values = [];
+this.tooltipId = undefined;
 
 for(var i = 0; initialHeights.length > i; i++){
-  values.push(initialHeights[i].clientHeight)
+  values.push(initialHeights[i].clientHeight);
 }
 
-var mainSubMenus = document.getElementsByClassName("level-1")
+var mainSubMenus = document.getElementsByClassName("level-1");
 function toZero(){
-  for(var i = 0; mainSubMenus.length > i; i++){
-    mainSubMenus[i].setAttribute("style", "height:0;")
+  for(i = 0; mainSubMenus.length > i; i++){
+    mainSubMenus[i].setAttribute("style", "height:0;");
   }
 }
-toZero()
+toZero();
 
 var toggleNav = function () {
   
-  var sections = document.querySelectorAll('span.menu-label.list')
+  var sections = document.querySelectorAll('span.menu-label.list');
   for(var i=0; i < sections.length; i++){
-    sections[i].classList.remove('active')
+    sections[i].classList.remove('active');
   }
   // 'this' is the target element that the click event was bound to.
 
   // get the height of all of the inner UL elements within the parent element
-  var temp = document.getElementById('flyout')
+  var temp = document.getElementById('flyout');
 
-  for(var i = 0; mainSubMenus.length > i; i++){
+  for(i = 0; mainSubMenus.length > i; i++){
     if(this.classList.contains("expanded")) {
-      toZero()
+      toZero();
     }
     else if(this.classList.contains("collapsed")) {
       if(temp.classList.contains('active')){
-        temp.classList.remove('active')
+        temp.classList.remove('active');
          tooltipId = window.setTimeout(countdown, 250);
       }
       // make sure all uf the child lists are collapsed
-      for(var i = 0; initialHeights.length > i; i++){
-        initialHeights[i].classList.remove('expanded')
-        initialHeights[i].classList.add('collapsed')
+      for(i = 0; initialHeights.length > i; i++){
+        initialHeights[i].classList.remove('expanded');
+        initialHeights[i].classList.add('collapsed');
       }
-      toZero()
+      toZero();
     }
   }
 
   // identify the parent element of the menu
-  var parentElement = document.getElementById('navigation')
+  var parentElement = document.getElementById('navigation');
   
   // alter the classList object to toggle classes for css animation/transition !IE9
-  parentElement.classList.toggle("collapsed")
-  parentElement.classList.toggle("expanded")
+  parentElement.classList.toggle("collapsed");
+  parentElement.classList.toggle("expanded");
   
   // do the same to the button so the inline svg can adapt to the state
-  this.classList.toggle("collapsed")
-  this.classList.toggle("expanded")
+  this.classList.toggle("collapsed");
+  this.classList.toggle("expanded");
 
 };
 
@@ -63,119 +63,116 @@ var toggleNav = function () {
 nav.addEventListener('click', toggleNav, false);
 
 // Accordion action bound to the main section element name 'menu-label.list'
-var control = document.getElementById('navigation')
+var control = document.getElementById('navigation');
 
 function hideToolltip(){
-  document.getElementById('menu_tooltip').classList.remove('tooltip')
-  document.getElementById('menu_tooltip').classList.add('hidden')
-  document.getElementById('menu_tooltip').innerHTML = ''
+  document.getElementById('menu_tooltip').classList.remove('tooltip');
+  document.getElementById('menu_tooltip').classList.add('hidden');
+  document.getElementById('menu_tooltip').innerHTML = '';
 }
 
 var menu_tooltip = function(event, str) {
   window.clearTimeout(tooltipId);
-  var element = event.path[0]
+  var element = event.path[0];
   if(document.getElementById('navigation').classList.contains('collapsed')) {
     // show tooltip
     if(!document.getElementById('flyout').classList.contains('active')){
-      var useThis = document.getElementById('menu_tooltip')
-      useThis.style.top = '-100px'
-      var elem = element.getBoundingClientRect()
-      var elemTop = elem.top
-      var elemHeight = elem.height / 2
-      var elemWidth = elem.width
-      useThis.classList.remove('hidden')
-      useThis.classList.add('tooltip')
-      useThis.innerHTML = ''
-      useThis.style.top = elemTop + 6 + 'px'
-      useThis.innerHTML = str
+      var useThis = document.getElementById('menu_tooltip');
+      useThis.style.top = '-100px';
+      var elem = element.getBoundingClientRect();
+      var elemTop = elem.top;
+      var elemHeight = elem.height / 2;
+      var elemWidth = elem.width;
+      useThis.classList.remove('hidden');
+      useThis.classList.add('tooltip');
+      useThis.innerHTML = '';
+      useThis.style.top = elemTop + 6 + 'px';
+      useThis.innerHTML = str;
       tooltipId = window.setTimeout(hideToolltip, 2000);
     }
   }
-}
-
+};
 
 var accordion = function(name, event) {
-
   
-  var clickedSections = document.querySelectorAll('span.menu-label.list')
-  for(var i=0; i < clickedSections.length; i++){
-    if(event.path[1].classList.contains('active')) {
-      clickedSections[i].classList.remove('active')
-      document.getElementById('flyout').classList.remove('active')
-      return
+  var clickedSections = document.querySelectorAll('span.menu-label.list');
+  for(i=0; i < clickedSections.length; i++){
+    if(event.path[1].classList.contains('active') && document.getElementById('flyout').classList.contains('active')) {
+      event.path[1].classList.remove('active');
+      document.getElementById('flyout').classList.remove('active');
+      return;
     }
-    clickedSections[i].classList.remove('active')
+    clickedSections[i].classList.remove('active');
   }
-  var thisSection = event.path[1].classList.add('active')
+  var thisSection = event.path[1].classList.add('active');
   // tooltipId = window.setTimeout(hideToolltip, 0);
-  document.getElementById('menu_tooltip').classList.remove('tooltip')
-  document.getElementById('menu_tooltip').classList.add('hidden')
+  document.getElementById('menu_tooltip').classList.remove('tooltip');
+  document.getElementById('menu_tooltip').classList.add('hidden');
 
   // Main accordion functionality - only available when expanded
   if(document.getElementById('navigation').classList.contains('expanded')){ // key element for differentiating between states
-    var menuToUse = document.getElementById(name), indexOfMenu
-    var allMenusReset = document.querySelectorAll('ul.level-1')
+    var menuToUse = document.getElementById(name), indexOfMenu;
+    var allMenusReset = document.querySelectorAll('ul.level-1');
     if (menuToUse.classList.contains('collapsed')){
-      for(var i=0; i < allMenusReset.length; i++){
-        allMenusReset[i].style.height = '0px'
-        allMenusReset[i].classList.remove('expanded')
-        allMenusReset[i].classList.add('collapsed')
-        allMenusReset[i].setAttribute('dataIndex', i)
+      for(i=0; i < allMenusReset.length; i++){
+        allMenusReset[i].style.height = '0px';
+        allMenusReset[i].classList.remove('expanded');
+        allMenusReset[i].classList.add('collapsed');
+        allMenusReset[i].setAttribute('dataIndex', i);
       }
       if(menuToUse.getAttribute('style') !== "height:0px;"){
-        indexOfMenu = menuToUse.getAttribute('dataIndex')
+        indexOfMenu = menuToUse.getAttribute('dataIndex');
         if (menuToUse.classList.contains('collapsed')){
-          menuToUse.setAttribute("style", "height: calc("+ values[indexOfMenu] +"px + 15px);")
-          menuToUse.classList.remove('collapsed')
-          menuToUse.classList.add('expanded')
+          menuToUse.setAttribute("style", "height: calc("+ values[indexOfMenu] +"px + 15px);");
+          menuToUse.classList.remove('collapsed');
+          menuToUse.classList.add('expanded');
         }
       }
       else {
-        indexOfMenu = menuToUse.getAttribute('dataIndex')
-        menuToUse.setAttribute("style", "height: calc("+ values[indexOfMenu] +"px + 15px);")
-        menuToUse.classList.remove('collapsed')
-        menuToUse.classList.add('expanded')
+        indexOfMenu = menuToUse.getAttribute('dataIndex');
+        menuToUse.setAttribute("style", "height: calc("+ values[indexOfMenu] +"px + 15px);");
+        menuToUse.classList.remove('collapsed');
+        menuToUse.classList.add('expanded');
       }
     }
     else {
-      menuToUse.setAttribute("style", "height:0px;")
-      menuToUse.classList.remove('expanded')
-      menuToUse.classList.add('collapsed')
+      menuToUse.setAttribute("style", "height:0px;");
+      menuToUse.classList.remove('expanded');
+      menuToUse.classList.add('collapsed');
     }
 
   }
   // fly-out menus for when the menu is collapsed
   else {
-    var temp = document.getElementById('flyout') || ''
+    var temp = document.getElementById('flyout') || '';
     if(event.path[0].classList.contains('active') && temp.classList.contains('active')){
-      console.log('line 103')
       tooltipId = window.setTimeout(countdown, 25);
-      temp.innerHTML = ''
-      clickedSections[i].classList.remove('active')
-      temp.classList.remove('active')
-      return
+      temp.innerHTML = '';
+      event.path[0].classList.remove('active');
+      temp.classList.remove('active');
+      return;
     }
     else{
       window.clearTimeout(tooltipId);
-      var clickedElement = event.path[1].children[1].cloneNode(true)
-      var nextElement = document.getElementById(name).cloneNode(true)
-      nextElement.id = '_'+name // alter the id to avoid clashes
-      temp.classList.add('active')
-      temp.innerHTML = ''
-      temp.appendChild(clickedElement)
-      temp.appendChild(nextElement)
+      var clickedElement = event.path[1].children[1].cloneNode(true);
+      var nextElement = document.getElementById(name).cloneNode(true);
+      nextElement.id = '_'+name; // alter the id to avoid clashes
+      temp.classList.add('active');
+      temp.innerHTML = '';
+      temp.appendChild(clickedElement);
+      temp.appendChild(nextElement);
       tooltipId = window.setTimeout(countdown, 3000);
     }
   }
-}
+};
   
 var countdown = function(){
-  document.getElementById('flyout').classList.remove('active')
-  document.getElementById('flyout').innerHTML = ''
+  document.getElementById('flyout').classList.remove('active');
+  document.getElementById('flyout').innerHTML = '';
   if(document.querySelectorAll('span.menu-label.list').classList.contains('active'))
-    document.querySelectorAll('span.menu-label.list').classList.remove('active')
-  return true
-}
+    document.querySelectorAll('span.menu-label.list').classList.remove('active');
+  return true;
+};
 
 function hideflyout(event){
   var x = event.clientX;
